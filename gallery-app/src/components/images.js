84eea1d795c4600f905ebe066f68fc2692e065fc
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-07-06 20:19:56 +0100

    added function imageClick

--- gallery-app/src/components/images.js
+++ gallery-app/src/components/images.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import '../index.css';
 
+import imageData from '../imageData';
+
+function imageClick(){
+    const imageArray = imageData.data.gallery
+    const randomNumber = Math.floor(Math.random() * imageArray.length)
+    const url = imageArray[randomNumber].url
+}
+
 export default function Images(){
     return(
         <div className='image-div'>
