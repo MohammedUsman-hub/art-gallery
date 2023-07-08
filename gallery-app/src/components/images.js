17e918a9e90c5ae550cdb25c55cdfa9d73f767c6
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-07-08 23:34:46 +0100

    changing some objects

--- gallery-app/src/components/images.js
+++ gallery-app/src/components/images.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import '../index.css';
 
-import imageData from '../imageData';
+import imageData from '../imageData.js';
 
 function imageClick(){
     const imageArray = imageData.data.gallery
     const randomNumber = Math.floor(Math.random() * imageArray.length)
-    const url = imageArray[randomNumber].url
+    const img = imageArray[randomNumber].img
 }
 
 export default function Images(){
@@ -15,7 +15,7 @@ export default function Images(){
     function getImageData(){
     const imageArray = imageData.data.gallery
     const randomNumber = Math.floor(Math.random() * imageArray.length)
-    setimgData(imageArray[randomNumber].url)
+    setimgData(imageArray[randomNumber].img)
 }
     
     return(
