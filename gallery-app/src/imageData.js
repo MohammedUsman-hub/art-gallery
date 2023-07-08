0f2bcb44f4de10d9866eae8735e4d7b8bc59adf6
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-07-09 00:31:30 +0100

    brk

--- gallery-app/src/imageData.js
+++ gallery-app/src/imageData.js
@@ -6,52 +6,52 @@ export default {
             {
                 id: 1,
                 title: "art1",
-                img: "art1.jpg",
+                img: "art1.jpg"
             },
             {
                 id: 2,
                 title: "art2",
-                img: "art2.jpg", 
+                img: "art2.jpg" 
             },
             {
                 id: 3,
                 title: "art3",
-                img: "art3.jpg",
+                img: "art3.jpg"
             },
             {
                 id: 4,
                 title: "art4",
-                img: "art4.jpg",
+                img: "art4.jpg"
             },
             {
                 id: 5,
                 title: "art5",
-                img: "art5.jpg",
+                img: "art5.jpg"
             },
             {
                 id:6,
                 title: "art6",
-                img: "art6.jpg",
+                img: "art6.jpg"
             },
             {
                 id: 7,
                 title: "art7",
-                img: "art7.jpg",
+                img: "art7.jpg"
             },
             {
                 id: 8,
                 title: "art8",
-                img: "art8.jpg",
+                img: "art8.jpg"
             },
             {
                 id: 9,
                 title: "art9",
-                img: "art9.jpg",
+                img: "art9.jpg"
             },
             {
                 id: 10,
                 title: "art10",
-                img: "art10.jpg",
+                img: "art10.jpg"
             },
 
         ]
